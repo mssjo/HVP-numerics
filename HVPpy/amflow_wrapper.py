4a8010f8c6a9b51bed5405583f7b2121d514e4e9
mssjo <mattias.s.sjo@gmail.com>
2026-03-02 19:48:13 +0100

    Fix broken call to AMFlow (still problematic)

--- HVPpy/amflow_wrapper.py
+++ HVPpy/amflow_wrapper.py
@@ -12,68 +12,67 @@ from .integration import QuadError
 
 class AMFlow:
 
-    def __init__(self, nu, *, dim="4", force=False, verbose=False, **kwargs):
-        self.verbose = verbose
-        if self.verbose:
-            print(f"Processing {nu=}{' (forced)' if force else ''}", end=' ')
+    def __init__(self, n, *, dim="4", force=False, **kwargs):
+        self.nu = nu_master[n]
+        clogger.debug(f"Processing {self.nu=}{' (forced)' if force else ''}")
+
+        # Due to the need to embed t in integral.wl we must defer a lot of things
+        #  to __call__, but we keep the psd_wrapper semantics
+        self.dim = dim
+        self.kwargs = kwargs
 
         # Setup the key used to refer to the integral
-        n_loop = get_n_loop(nu)
-        self.key = encode(nu, dim, 0, name='I')
-        if verbose:
-            print(f"({n_loop=}, {self.key=})")
+        n_loop = get_n_loop(self.nu)
+        self.key = encode(self.nu, dim, 0, name='I')
+        clogger.debug(f"({n_loop=}, {self.key=})")
 
         # Clear existing directory if forced
-        if not os.path.exists(self.path()):
-            clogger.info(f"[AMFlow] Creating directory {self.path()}")
-            os.makedirs(self.path(), exist_ok=True)
         if force:
             if subprocess.run(["rm", "-rf", f"./{self.path()}"]).returncode:
                 raise IOError(f"[AMFlow] Failed to remove directory for integral {self.key}")
-
-        with open(f"{self.path()}integral.wl", 'w') as math:
-            print(f'SetDirectory["{os.getcwd()}/{self.path()}"];', file=math);
-            print(f'Get["../AMFlow_defs.m"];', file=math);
-            print(f'SetAMFOptions["D0" -> {dim}];', file=math);
-            print(f'integral = j[HVP{n_loop}loop, {','.join((str(n) for n in nu))}];', file=math);
-            print(f'Compute[integral];', file=math);
-
-            clogger.debug(f"Generated {math.name}")
-
+        if not os.path.exists(self.path()):
+            clogger.info(f"[AMFlow] Creating directory {self.path()}")
+            os.makedirs(self.path(), exist_ok=True)
 
     def __enter__(self):
-        if self.verbose:
-            clogger.info(f"[AMFlow] Preparing run of {self.key}...")
+        clogger.debug(f"[AMFlow] Preparing run of {self.key}...")
         return self
 
     def __exit__(self, exc_type, exc, traceback):
         self.integral = None
 
-    def __call__(self, t, *, prec=mp.prec):
+    def __call__(self, t, *, prec=None):
 
-        # This is a workaround since wolframscript drops command-line arguments
-        with contextlib.chdir(self.path()):
-            with open("t.m", 'w') as out:
-                print(f"{t}", file=out)
-            with open("prec.m", 'w') as out:
-                print(f"{prec}", file=out)
+        if prec is None:
+            prec = mp.dps
+
+        with open(f"{self.path()}integral.wl", 'w') as math:
+            print('\n'.join((
+                f'SetDirectory["{os.getcwd()}/{self.path()}"];',
+                f'Get["../AMFlow_defs.m"];',
+                f'SetAMFOptions["D0" -> {self.dim}];',
+                f'integral = j[HVP{get_n_loop(self.nu)}loop, {','.join((str(n) for n in self.nu))}];',
+                f'Compute[integral, {t}, {prec}];',
+                f'Quit[];')),
+                file=math);
+
+            clogger.debug(f"Generated {math.name}")
+
 
         process = subprocess.run(
                 ["wolframscript", "-file", "integral.wl"],
                 cwd=self.path(),
-                capture_output=True, text=True
+                # capture_output=True, text=True
             )
+        clogger.debug(f"[AMFlow] Completed run of {self.key}.")
 
         if process.returncode or not os.path.isfile(f"{self.path()}integral.dat"):
             raise IOError(f"[AMFlow] Failed to compute {self.key} at t={t}")
 
-        for line in process.stderr.splitlines():
-            clogger.warning(f"[AMFlow] {line}")
-        for line in process.stdout.splitlines():
-            if self.verbose:
-                clogger.info(f"[AMFlow] {line}")
-            else:
-                clogger.debug(f"[AMFlow] {line}")
+        # for line in process.stderr.splitlines():
+        #     clogger.warning(f"[AMFlow] {line}")
+        # for line in process.stdout.splitlines():
+        #     clogger.debug(f"[AMFlow] {line}")
 
         with open(f"{self.path()}integral.dat", 'r') as raw:
             for line in raw:
@@ -100,14 +99,14 @@ class AMFlow:
 
                     token = token[:mark]
 
+                    # NOTE: opposite sign convention
                     if (mark := token.find('`')) != -1:
-                        result[eps] = QuadError(mpf(token[:mark]), mpf(f'1e-{token[mark+1:]}'))
+                        result[eps] = -QuadError(mpf(token[:mark]), mpf(f'1e-{token[mark+1:]}'))
                     else:
-                        result[eps] = QuadError(mpf(token))
+                        result[eps] = -QuadError(mpf(token))
 
-        if self.verbose:
-            clogger.debug(f"[AMFlow] {self.key}:")
-            clogger.debug(f"    {result}")
+        clogger.debug(f"[AMFlow] {self.key}:")
+        clogger.debug(f"    {result}")
 
         return result;
 
